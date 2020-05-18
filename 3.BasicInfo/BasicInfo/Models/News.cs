0731dea04cf7a8cd2a7ce9350c0e3dfcd280dce5
Ruslan Antoshkin <ra.antoshkin@gmail.com>
2020-05-18 17:14:24 +0300

    Added NewsBase

---
+++ 3.BasicInfo/BasicInfo/Models/News.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BasicInfo.Models
+{
+    public class News
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Text { get; set; }
+        public string AuthorName { get; set; }
+        public bool IsFake { get; set; }
+    }
+}
---
+++ 3.BasicInfo/BasicInfo/Models/NewsBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BasicInfo.Models
+{
+    public static class NewsBase
+    {
+        public static List<News> News = new List<News>
+        {
+            new News { Id = 0, Title = "Humanity finally colonized the Mercury!!", Text = "", AuthorName = "Jeremy Clarkson", IsFake = true},
+            new News { Id = 1, Title = "Increase your lifespan by 10 years, every morning you need...", Text = "", AuthorName = "Svetlana Sokolova", IsFake = true},
+            new News { Id = 2, Title = "Scientists estimed the time of the vaccine invension: it is a summer of 2021", Text = "", AuthorName = "John Jones", IsFake = false},
+            new News { Id = 3, Title = "Ukraine reduces the cost of its obligations!", Text = "", AuthorName = "Cerol Denvers", IsFake = false},
+            new News { Id = 4, Title = "A species were discovered in Africa: it is blue legless cat", Text = "", AuthorName = "Jimmy Felon", IsFake = true}
+        };
+    }
+}
