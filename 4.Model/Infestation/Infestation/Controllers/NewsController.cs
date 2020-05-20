c25db620a8e7d4f14c549eb811732aae3c4c380f
Ruslan Antoshkin <ra.antoshkin@gmail.com>
2020-05-21 02:11:24 +0300

    Added news to Infestation

---
+++ 4.Model/Infestation/Infestation/Controllers/NewsController.cs
@@ -0,0 +1,32 @@
+﻿using Infestation.Models.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Infestation.Controllers
+{
+    public class NewsController : Controller
+    {
+        private INewsRepository _repository { get; set; }
+
+        public NewsController(INewsRepository repository)
+        {
+            _repository = repository;
+        }
+
+
+        public IActionResult Index()
+        {
+            ViewData["key"] = _repository.GetAllNews();
+            return View();
+        }
+
+        public IActionResult Show(int newsId)
+        {
+            ViewData["key"] = _repository.GetAllNews().SingleOrDefault(news => news.Id == newsId);
+            return View();
+        }
+    }
+}
